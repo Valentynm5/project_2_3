95393987352bea2a091a60055659eb7072b4795e
Valentynm5 <valentynm5cs@gmail.com>
2023-04-05 11:03:03 +0300

    big

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter a number";
+    cin >> a;
+if (a % 2 == 0)
+{
+    cout << "Parne";
+}else {
+    cout << " Ne parne";
+}
+}
